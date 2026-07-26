cb52c8e7732fd7055234e4d2b6e005bfd2653b18
Victor Staver <staverviktor17@gmail.com>
2026-07-26 17:48:18 +0300

    feat: add case 2 author filter test

--- test_practice_4_6.py
+++ test_practice_4_6.py
@@ -37,3 +37,33 @@ def test_github_issues_search():
     # 5. Пауза 5 секунд для визуальной проверки
     time.sleep(5)
     driver.quit()
+
+def test_github_author_filter():
+    """Кейс №2: Фильтрация по автору через выпадающее меню Author"""
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    driver.get('https://github.com/microsoft/vscode/issues')
+
+    wait = WebDriverWait(driver, 10)
+
+    # 1. Открываем выпадающее меню Author
+    author_button = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'authorFilterButton')]"))
+    )
+    author_button.click()
+
+    # 2. Вводим имя автора bpasero в поле поиска авторов
+    author_input = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Filter authors']"))
+    )
+    author_input.send_keys("bpasero")
+
+    # 3. Выбираем найденного автора из списка по точному тексту
+    author_option = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//*[text()='author:bpasero']"))
+    )
+    author_option.click()
+
+    # 4. Пауза 5 секунд для визуальной проверки
+    time.sleep(5)
+    driver.quit()
